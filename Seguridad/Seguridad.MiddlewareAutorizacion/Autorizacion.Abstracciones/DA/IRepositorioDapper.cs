093aee0adcd44c938c2e5a3193f8622ff7be2a97
StimpyCR <marianojosegp@gmail.com>
2026-03-18 16:08:36 -0600

    Practica 7

--- Seguridad/Seguridad.MiddlewareAutorizacion/Autorizacion.Abstracciones/DA/IRepositorioDapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.SqlClient;
-
-namespace Autorizacion.Abstracciones.DA
-{
-    public interface IRepositorioDapper
-    {
-        SqlConnection ObtenerRepositorioDapper();
-    }
-}
